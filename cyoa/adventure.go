168097f26b27227c5c70a51d63490d5e84778f92
J Nguhy <ndnguhi@gmail.com>
2023-02-07 15:42:24 +0300

    adds path parsing for my http handler

--- cyoa/adventure.go
+++ cyoa/adventure.go
@@ -3,7 +3,9 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -42,10 +44,25 @@ type handler struct {
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	err := temp.Execute(w, h.story["intro"])
-	if err != nil {
-		panic(err)
+
+	path := strings.TrimSpace(r.URL.Path)
+
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+
+	path = path[1:]
+
+	if chapter, ok := h.story[path]; ok {
+		err := temp.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Unexpected Error happened", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	http.Error(w, "Not Found", http.StatusNotFound)
 }
 
 func NewHandler(s Story) http.Handler {
